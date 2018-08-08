a26a94e2a33fc1aaa6aebd006016370e310188c6
DESKTOP-30EOSUL\李佳杰 <1336706715@qq.com>
2018-08-08 14:53:23 +0800

    ljiajie ennemyfish 【1】增强鱼的随机性 【2】增加守卫点 【3】墨汁变为远程攻击

--- proj.win32/EnemyFish.h
+++ proj.win32/EnemyFish.h
@@ -9,18 +9,27 @@
 #define FISHHEALTHY1 100
 #define ALARMDISTANCE1 300
 #define ATTACK_PROB 8
+#define GUARD_POSTION_DISTANCE 800
 
 class EnemyFish : public GeneralUnit
 {
 public:
 	float TIME_FOR_ANIMATION_FRAME_INTERVAL = 0.2f;
+	float ANIMATION_FRAME_INTERVAL = 0.3f;
 	float speed_flying_pixel_per_second = 100;
-	
+	int damage = 10;
+
+	Sprite* ink_con;
+
 	bool acceptCall = true;
 	//小怪状态
 	bool facingRight = true;
 	bool facingLeft = false;
 	bool moving = false;
+
+	//守卫点
+	int guardPostionX = 6850;
+	int guardPostionY = -200;
 
 	//悬停动画
 	Animation * hoveringRightAnimation;
@@ -38,6 +47,8 @@ public:
 
 	//攻击动画
 	Animation * attackAnimation;
+	Animation * newAttackAnimation;
+	Animation * inkBallAttackAnimation;
 
 	//受伤动画
 	Animation *getHurtAnimation;
@@ -51,8 +62,20 @@ public:
 	static EnemyFish * create(const std::string & filename);
 
 	void wanderAbout();
+	//void wanderAbout(int guardPosX, int guardPosY);
 	void getHurt();
 	void getHurt(int damage);
 	void getHurtByWind(int damage);
+
+	//鸟态主角
+	void getHeld(int);
+	void 	getThrown();
+	void getSlamDunkOnGround(int);
+	void getSlamDunkOnWater(int);
+
+	//鱼态主角
+	void 	getCollided(int);
+
+
 	void die();
 };
